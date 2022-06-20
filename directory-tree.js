3379063d5357435f85376c3fa5e412456c35a3ad
miriam <miriam_glzr@hotmail.com>
2022-06-20 23:31:53 +0200

    final commit

--- directory-tree.js
+++ directory-tree.js
@@ -2,8 +2,8 @@ const path = require("path");
 const fs = require("fs");
 
 const listFiles = (dir) => {
-	console.log(dir);
 	fs.readdir(path.join(__dirname, dir), (err, files) => {
+		if (dir[0] === ".") console.log(dir);
 		if (err) {
 		} else if (files.length) {
 			files.forEach((file) => {
